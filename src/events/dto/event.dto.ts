aa6fb1fd30d86fb8b38cde706b38e50c638d9175
Jeferson Delgado <92939552+jeferD@users.noreply.github.com>
2023-08-31 07:53:51 -0500

    first commit

---
+++ src/events/dto/event.dto.ts
@@ -0,0 +1,35 @@
+import { IsNotEmpty, IsDate, IsUrl, ValidateIf, Min, ValidationOptions } from "class-validator";
+
+export class EventDto {
+    id?: string;
+    @IsNotEmpty()
+    name: string;
+    @IsNotEmpty()
+    description: string;
+    @IsDate()
+    date: string;
+    @IsUrl()
+    images: string;
+
+    @IsNotEmpty()
+    owner: string;
+
+    @ValidateIf((_,value)=> value != undefined)
+    address?: string;
+
+    // @ValidateIf((_,value)=> value != undefined)
+    @IsNull()
+    @Min(0)
+    cost?: number;
+}
+
+
+
+function IsNull(validationOptions?: ValidationOptions){
+    return ValidateIf((_,value)=> value != undefined, validationOptions)
+}
+
+
+
+
+
---
+++ src/events/events.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { EventDto } from './dto/event.dto';
+
+@Injectable()
+export class EventsService {
+    events: EventDto[] = [];
+
+    add(event: EventDto){
+        this.events.push(event)
+    }
+
+    update(id:string, event:EventDto){
+        const index =  this.events.findIndex((e)=>e.id = id)
+        if(index == undefined){
+            return
+        }
+        this.events.splice(index,1)
+    }
+
+    remove(id: string){
+        const index =  this.events.findIndex((e)=>e.id = id)
+        if(index == undefined){
+            return
+        }
+        this.events.splice(index,1)
+    }
+
+    get(page: number, pageSize:number): EventDto[]{
+        return this.events;
+    }
+
+    getById(id:string): EventDto | undefined{
+        return this.events.find((e)=> e.id == id);
+    }
+}
